7082fb72da3f76e69abe144ffa51a7f643f04518
Hack Reactor Students <accounts@hackreactor.com>
2016-08-24 23:33:02 -0700

    Last of the night

--- Gruntfile.js
+++ Gruntfile.js
@@ -51,6 +51,10 @@ module.exports = function(grunt) {
 
     shell: {
       prodServer: {
+        command: [
+          'cd ~/Desktop/shortly-deploy',
+          'git push live master'
+        ].join('&&')
       }
     },
   });
@@ -79,8 +83,16 @@ module.exports = function(grunt) {
   grunt.registerTask('build', [
   ]);
 
+  grunt.registerTask('push', [
+    'shell:prodServer:command'
+  ]);
+
+
   grunt.registerTask('upload', function(n) {
     if (grunt.option('prod')) {
+
+      
+
       // add your production server task here
     } else {
       grunt.task.run([ 'server-dev' ]);
